ca167acb12f8059aa5ab67ff4aa7c8e46592f0e9
Andrea Lovewell <andy.lovewell@gmail.com>
2019-01-31 06:46:44 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+  def initialize (name)
+    @name = name 
+  end
+
+  def name
+    @name
+  end
+
+  def initialize(breed)
+    @breed = breed
+    
+  def breed 
+    @breed
+  end
+end
+
+fido = Dog.new
